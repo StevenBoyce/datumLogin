7e8594fa965fa1a43eb1c7eb6cd0f9ecb63ba83e
Steven Boyce <steven.boyce@gabbwireless.com>
2020-11-12 09:25:16 -0700

    extracting the js

---
+++ script.js
@@ -0,0 +1,33 @@
+window.onload = function () {
+  var Router = function (name, routes) {
+    return {
+      name,
+      routes,
+    };
+  };
+
+  var view = document.getElementById("view");
+
+  var router = new Router("router", [
+    {
+      path: "/",
+      name: "Login",
+    },
+    {
+      path: "/sign-up",
+      name: "SignUp",
+    },
+    {
+      path: "/reset-password",
+      name: "ResetPassword",
+    },
+  ]);
+  var currentPath = window.location.pathname;
+  if (currentPath === "/") {
+    view.innerHTML = "This is the login page";
+  } else {
+    var route = router.routes.filter(function (r) {
+      return r.path === currentPath;
+    })[0];
+  }
+};
